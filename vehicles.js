d16280c1e2664e207ee404737876f1db403f7d3a
alexm <alex-m@outlook.com>
2022-10-26 21:32:34 -0700

    Added code for Vehicle class, with constructor, honk() method, and toString() method

--- vehicles.js
+++ vehicles.js
@@ -2,3 +2,20 @@
 // 10/26/22
 // Exercise 12.1.12: Car OO Project
 
+
+/* Represents any vehicle. */
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    honk() {
+        return "Beep.";
+    }
+
+    toString() {
+        return `The vehicle is a ${make} ${model} from ${year}`;
+    }
+}
